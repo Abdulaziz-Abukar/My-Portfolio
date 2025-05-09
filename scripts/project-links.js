619bd3c62ef694adb27ecaba305b1eec51bc23d8
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-05-09 03:07:39 -0600

    adds new project

--- scripts/project-links.js
+++ scripts/project-links.js
@@ -1,14 +1,14 @@
 const projectLinks = [
-    "https://abdulaziz-abukar.github.io/Interactive-World-Map/",
-    "https://github.com/Abdulaziz-Abukar/vacation-planner",
-    "https://apps.apple.com/vn/app/chek-productivity-app/id6470704114"
-  ];
-  
-  const projectCards = document.querySelectorAll(".projects");
-  
-  projectCards.forEach((card, index) => {
-    card.addEventListener("click", () => {
-      window.open(projectLinks[index], "_blank");
-    });
+  "https://abdulaziz-abukar.github.io/Interactive-World-Map/",
+  "https://github.com/Abdulaziz-Abukar/vacation-planner",
+  "https://apps.apple.com/vn/app/chek-productivity-app/id6470704114",
+  "https://abdulaziz-abukar.github.io/jammming/",
+];
+
+const projectCards = document.querySelectorAll(".projects");
+
+projectCards.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    window.open(projectLinks[index], "_blank");
   });
-  
+});
