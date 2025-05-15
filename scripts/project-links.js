3d3fc3cab82e3a465081526c489ba1df075b6bfd
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-05-14 18:01:47 -0600

    fixes project section order and fixes responsiveness for mobile users on projects

--- scripts/project-links.js
+++ scripts/project-links.js
@@ -1,9 +1,9 @@
 const projectLinks = [
+  "https://abdulaziz-abukar.github.io/devfinder",
+  "https://abdulaziz-abukar.github.io/jammming/",
+  "https://apps.apple.com/vn/app/chek-productivity-app/id6470704114",
   "https://abdulaziz-abukar.github.io/Interactive-World-Map/",
   "https://github.com/Abdulaziz-Abukar/vacation-planner",
-  "https://apps.apple.com/vn/app/chek-productivity-app/id6470704114",
-  "https://abdulaziz-abukar.github.io/jammming/",
-  "https://abdulaziz-abukar.github.io/devfinder",
 ];
 
 const projectCards = document.querySelectorAll(".projects");
